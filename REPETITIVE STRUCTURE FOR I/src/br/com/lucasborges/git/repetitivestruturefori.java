2cc1ec41a63a03d9afbe0ffc4b34f5521d576825
Lucas Borges <76432049+DevLucasBorges@users.noreply.github.com>
2021-05-20 09:26:27 -0300

    Add files via upload

---
+++ REPETITIVE STRUCTURE FOR I/src/br/com/lucasborges/git/repetitivestruturefori.java
@@ -0,0 +1,21 @@
+package br.com.lucasborges.git;
+
+import java.util.Scanner;
+
+public class repetitivestruturefori {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+		
+		for (int i=1; i<=x; i++) {
+			if (i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		
+		sc.close();
+	}
+}
